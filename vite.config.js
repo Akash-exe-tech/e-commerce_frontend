dfe6c5b62fde901f4419f4eb9837134f0735146b
Akash Suresh <akash944686@gmail..com>
2025-07-09 16:48:54 +0530

    Setup .env handling and login API integration

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),         // homepage
-        register: resolve(__dirname, 'register.html'),   // register page
-      },
-    },
-  },
-});
+  plugins: [vue(),
+    tailwindcss(),
+  ],
+})
